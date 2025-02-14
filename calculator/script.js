57fdfcde7a731f357e8754bab17e786c283c642c
RYANN <yan1511006@gmail.com>
2025-02-14 20:06:25 +0700

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button'); 
+const specialChar = ["%","*","/","-","+","="];
+let output = "";
+
+const calculate = (btnValue) => {
+    display.focus();
+    if (btnValue === '=' && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "DEL") {
+        output = output.slice(0, -1); 
+    } else if (btnValue === "AC") {
+        output = ""; 
+    } else {
+        if (output === "" && specialChar.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => calculate(e.target.dataset.value));
+});
